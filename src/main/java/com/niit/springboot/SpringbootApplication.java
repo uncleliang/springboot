54dc0c8b630c9f169deff294a710bb05517e23bc
hanliang <hanliangsoft@126.com>
2019-09-26 15:28:36 +0800

    Initial commit

---
+++ src/main/java/com/niit/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.niit.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
